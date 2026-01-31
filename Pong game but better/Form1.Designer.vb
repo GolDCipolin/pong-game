ada260872ddaba4ca2b32420b08fed30b3896c05
GolDCipolin <123726815+GolDCipolin@users.noreply.github.com>
2026-01-31 00:21:01 +0000

    game upload

---
+++ Pong game but better/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Pong
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Bat = New System.Windows.Forms.Button()
+        Me.Ball = New System.Windows.Forms.Button()
+        Me.Scorebox = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Bat
+        '
+        Me.Bat.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Bat.Location = New System.Drawing.Point(41, 500)
+        Me.Bat.Name = "Bat"
+        Me.Bat.Size = New System.Drawing.Size(100, 20)
+        Me.Bat.TabIndex = 0
+        Me.Bat.UseVisualStyleBackColor = False
+        '
+        'Ball
+        '
+        Me.Ball.Location = New System.Drawing.Point(374, 245)
+        Me.Ball.Name = "Ball"
+        Me.Ball.Size = New System.Drawing.Size(10, 10)
+        Me.Ball.TabIndex = 1
+        Me.Ball.UseVisualStyleBackColor = True
+        '
+        'Scorebox
+        '
+        Me.Scorebox.AutoSize = True
+        Me.Scorebox.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Scorebox.ForeColor = System.Drawing.Color.White
+        Me.Scorebox.Location = New System.Drawing.Point(391, 9)
+        Me.Scorebox.Name = "Scorebox"
+        Me.Scorebox.Size = New System.Drawing.Size(29, 31)
+        Me.Scorebox.TabIndex = 2
+        Me.Scorebox.Text = "0"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 70
+        '
+        'Pong
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.ForestGreen
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.Scorebox)
+        Me.Controls.Add(Me.Ball)
+        Me.Controls.Add(Me.Bat)
+        Me.Name = "Pong"
+        Me.Text = "Pong"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Bat As Button
+    Friend WithEvents Ball As Button
+    Friend WithEvents Scorebox As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Pong game but better/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Pong
+    Dim x1, ballx, bally, xspeed, yspeed, score As Integer
+    Dim rand As New Random
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ballx > 770 Then xspeed = -10
+        If ballx < 10 Then xspeed = 10
+        If bally < 10 Then yspeed = 10
+        If bally > 480 Then
+            If ballx > x1 - 60 And ballx < x1 + 50 Then
+                yspeed = -10
+                score = score + 1
+                Scorebox.Text = score
+                If Timer1.Interval > 10 Then Timer1.Interval = Timer1.Interval - 5
+            Else
+                Timer1.Enabled = False
+                MsgBox("The End! Score was" & score)
+                Close()
+            End If
+        End If
+        ballx = ballx + xspeed
+        bally = bally + yspeed
+        Ball.Left = ballx
+        Ball.Top = bally
+    End Sub
+
+    Private Sub Pong_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        xspeed = rand.Next(2)
+        If xspeed >= 1 Then xspeed = 10 Else xspeed = -10
+        yspeed = -10
+        ballx = rand.Next(400)
+        bally = 460
+        score = 0
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Pong_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        x1 = e.X
+        Bat.Left = x1 - 50
+
+    End Sub
+End Class
